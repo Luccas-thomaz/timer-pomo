cfc2500837dea89cfa9c497f54595da2ab937cb6
Luccas-thomaz <108094478+Luccas-thomaz@users.noreply.github.com>
2024-06-22 22:04:43 -0300

    att som
    adicionado som ao terminar o timer e ao clicar no botão

--- Scripts/Timer.js
+++ Scripts/Timer.js
@@ -1,10 +1,15 @@
 let timer;
-let timeLeft = 25 * 60;
+//let timeLeft = 25 * 60;//coloca o timer para 25 min
+let timeLeft = 5 //coloca o timer em 5 segundo para teste
 let isRunning = false;
 
-const display = document.getElementById('Timer');
-const startButton = document.getElementById('Start');
-const resetButton = document.getElementById('Stop');
+const display = document.getElementById('Timer');//pega o timer
+const startButton = document.getElementById('Start');//pega o start button
+const resetButton = document.getElementById('Reset');//pega o Reset button
+const stopButton = document.getElementById('Stop');//pega o stop button
+const audio = document.getElementById('audioElement');//pega o alarme
+const apertar = document.getElementById('audioClick'); //pega o click
+
 
 function updateDisplay() {
     const minutes = Math.floor(timeLeft / 60);
@@ -20,7 +25,15 @@ function startTimer() {
         if (timeLeft <= 0) {
             clearInterval(timer);
             isRunning = false;
-            alert('Tempo esgotado!');
+            audio.play();
+            resetTimer;
+            setTimeout(() => {
+                alert('Tempo esgotado!');
+                timeLeft = 25*60;
+                updateDisplay();
+            }, 100);
+        
+           
         } else {
             timeLeft--;
             updateDisplay();
@@ -34,8 +47,25 @@ function resetTimer() {
     timeLeft = 25 * 60;
     updateDisplay();
 }
+function stopTimer(){
+    clearInterval(timer);
+    isRunning = false;
+    audio.pause();
+}
+
+function playButtonSound() {
+    apertar.currentTime = 0; // Reiniciar o som para o início
+    apertar.play().catch(error => {
+        console.log("Erro ao tocar o som de clique:", error);
+    });
+}
 
 startButton.addEventListener('click', startTimer);
 resetButton.addEventListener('click', resetTimer);
+stopButton.addEventListener('click',stopTimer);
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', playButtonSound);
+});
 updateDisplay();
